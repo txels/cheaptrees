40726e28ea74f9d10b38298c977fece2ad2b8704
Carles Barrobés <carles.barrobes@uktv.co.uk>
2012-09-23 00:42:16 +0100

    Raise exception for unsupported more than one digit

--- cheaptrees/encode.py
+++ cheaptrees/encode.py
@@ -13,18 +13,20 @@ class EncoderException(Exception):
 
 
 class Encoder(object):
+
     def __init__(self, base=10, start_char='0'):
         self.base = base
         # number of bits needed to represent a number in the given base:
         numbits = needed_bits(base)
         self.digits = int(math.ceil(numbits / MAX_BITS))
-        # if self.digits != 1:
-        #     raise EncoderException('More than 1 digit per level not supported')
         self.start_char = start_char
 
-    # naive base10 encodings
     def encode(self, position):
+        if self.digits != 1:
+            raise EncoderException('More than 1 digit per level not supported')
         return chr(position + ord(self.start_char))
 
     def decode(self, encoded):
+        if self.digits != 1:
+            raise EncoderException('More than 1 digit per level not supported')
         return ord(encoded) - ord(self.start_char)
